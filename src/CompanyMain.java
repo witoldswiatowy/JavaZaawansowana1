effacf64266bc68b23eb33eca6168ff42b1ca792
Witold Swiatowy <swiatowywitold@gmail.com>
2022-04-23 14:01:46 +0200

    add Secratary class

--- src/CompanyMain.java
+++ src/CompanyMain.java
@@ -1,14 +1,17 @@
 import company.Employee;
 import company.Manager;
+import company.Secratary;
 
 public class CompanyMain {
 
     public static void main(String[] args) {
         Employee employeeOne = new Employee("Jan", 200,10);
         Manager employeeTwo = new Manager("Bogdan", 250, 50, 50);
+        Secratary employeeThree = new Secratary("Ala", 230, 33);
 
 //        employeeOne.age = 50;
         employeeOne.show();
         employeeTwo.show();
+        employeeThree.show();
     }
 }
---
+++ src/company/Secratary.java
@@ -0,0 +1,14 @@
+package company;
+
+public final class Secratary extends Employee {
+
+    public Secratary(String name, int salary, int age) {
+        super(name, salary, age);
+//        System.out.println("Wywołuje konstruktor klasy Secratary z argumentami: " + name + " i " + salary + " i mam lat " + age);
+        System.out.printf("Wywołuje konstruktor klasy Secratary z argumentami: %s i %s i %s\n", name, salary, age);
+    }
+
+    public void makeCall(String number) {
+        System.out.printf("Dzwonie do " + number);
+    }
+}
